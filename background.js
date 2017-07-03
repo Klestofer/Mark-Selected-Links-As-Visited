312149fe29cbf6bf39aa1eb1e14a75287224a6d5
Klestofer <klestofer@gmail.com>
2017-07-04 00:33:53 +1000

    link context menu
    Added link context menu to 'visit' and 'unvisit' link.

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 'use strict';
+const l = console.log;
+const MENU_ID = '1';
 
 
 chrome.browserAction.onClicked.addListener(function(tab) {
@@ -12,4 +14,34 @@ chrome.runtime.onMessage.addListener(function(msg) {
   for(let url of msg) {
     chrome.history.addUrl({url});
   }
+});
+
+
+
+
+chrome.contextMenus.create({
+  title: 'Toggle visit state',
+  contexts: [ 'link' ],
+  id: MENU_ID
+}, function () { l('context menu created') });
+
+
+
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  l('contextMenus.onClicked()', info, tab);
+
+  const url = info.linkUrl;
+  chrome.history.getVisits({ url }, function({ length }) {
+    l(length);
+
+    if (length === 0) {
+      // link is not visited
+      chrome.history.addUrl({ url });
+    }
+    else {
+      // link is visited
+      chrome.history.deleteUrl({ url });
+    }
+  })
 });
